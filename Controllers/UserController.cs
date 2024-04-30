29034b20d2cc9ce7da60cfd0e2a7b70c92df1a85
LeoJrTheTanooki <leodevin4118@gmail.com>
2024-04-30 10:51:52 -0700

    AddUser implemented to UserController (Untested)

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Lvl3Week3Day2_BlogBackend.Models.DTO;
+using Lvl3Week3Day2_BlogBackend.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lvl3Week3Day2_BlogBackend.Controllers
@@ -10,6 +12,20 @@ namespace Lvl3Week3Day2_BlogBackend.Controllers
     [Route("[controller]")]
     public class UserController : ControllerBase
     {
+        private readonly UserService _data;
+
+        public UserController(UserService data)
+        {
+            _data = data;
+        }
+
         
+
+        [HttpPost]
+        [Route("AddUser")]
+        public bool AddUser(CreateAccountDTO UserToAdd)
+        {
+            return _data.AddUser(UserToAdd);
+        }
     }
 }
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -2,11 +2,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Lvl3Week3Day2_BlogBackend.Models;
+using Lvl3Week3Day2_BlogBackend.Models.DTO;
+using Lvl3Week3Day2_BlogBackend.Services.Context;
 
 namespace Lvl3Week3Day2_BlogBackend.Services
 {
     public class UserService
     {
-        
+
+        private readonly DataContext _context;
+
+        public UserService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public bool DoesUserExist(string Username){
+            return _context.UserInfo.SingleOrDefault(user => user.Username == Username) != null;
+        }
+
+        public bool AddUser(CreateAccountDTO UserToAdd)
+        {
+            bool result = false;
+
+            if(!DoesUserExist(UserToAdd.Username)){
+                UserModel newUser = new UserModel();
+                result = true;
+            }
+
+            return result;
+        }
     }
 }
